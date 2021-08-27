abdbeec73e707419e99e20ceb5b62cb891c6a371
Nguyen The Dat <67305993+ndat2408@users.noreply.github.com>
2021-08-27 14:32:22 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,29 @@
+var contentArray = ["Coder", "Designer", "Studient", "Web Developer"]
+var i=0
+
+var interval = setInterval(function(){
+    document.getElementById("txt").innerText = contentArray[i]
+    i++
+    if (i>=contentArray.length) i=0;
+},1000)
+
+
+// scroll ef
+window.onscroll = function(){
+    sticky = document.querySelector(".header").offsetTop
+    if (window.pageYOffset > sticky) {
+        document.querySelector(".header").classList.add("sticky");
+    } else {
+        document.querySelector(".header").classList.remove("sticky");
+    }
+}
+
+
+// menubtn
+document.getElementById("menu").onclick = function(){
+    document.querySelector("#navbar ul").style.left = 0;
+}
+
+document.querySelector("#navbar ul li:first-child").onclick = function(){
+    document.querySelector("#navbar ul").style.left = -100 + "%"
+}
